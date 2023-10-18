7af8fbabd61eefedb083ddab1489785e9b4e2c1e
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-18 21:00:18 +0530

    Login component completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AddClient from './Components/AddClient';
 import ViewClient from './Components/ViewClient';
 import SearchClient from './Components/SearchClient';
 import DeleteClient from './Components/DeleteClient';
+import Login from './Components/Login';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <div>
     <BrowserRouter>
     <Routes>
+      <Route path="/" exact element={<Login/>} />
       <Route path="/addg" exact element={<AddClient/>} />
       <Route path="/viewg" exact element={<ViewClient/>} />
       <Route path="/searchg" exact element={<SearchClient/>}/>
